3acc57fbc62b4b7031564ff80a5686b694c2ebb7
Danny Zolp <danny@zolp.dev>
2022-04-13 07:41:52 -0500

    added enemy obstacles

--- My project (1)/Assets/scripts/playerScript.cs
+++ My project (1)/Assets/scripts/playerScript.cs
@@ -42,19 +42,19 @@ public class playerScript : MonoBehaviour
     void Update()
     {
         //print(thePlayer.getName());
-        if (Input.GetKeyDown("up"))
+        if (Input.GetKeyDown("w"))
         {
             rb.velocity = Vector3.forward * speed;
         }
-        else if (Input.GetKeyDown("down"))
+        else if (Input.GetKeyDown("s"))
         {
             rb.velocity = Vector3.back * speed;
         }
-        else if (Input.GetKeyDown("left"))
+        else if (Input.GetKeyDown("a"))
         {
             rb.velocity = Vector3.left * speed;
         }
-        else if (Input.GetKeyDown("right"))
+        else if (Input.GetKeyDown("d"))
         {
             rb.velocity = Vector3.right * speed;
         }
